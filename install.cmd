96d58bc4e3426484b4d9869cd718ec88bb9b0118
Saadiq <101512821+Saadiq8149@users.noreply.github.com>
2025-07-05 22:56:12 +0530

    Changed install script

--- install.cmd
+++ install.cmd
@@ -1,1 +1,11 @@
+@echo off
+where python >nul 2>nul
+if %errorlevel% neq 0 (
+    echo Python is not installed on this system.
+    echo Please install Python before running this script.
+    pause
+    exit /b 1
+)
+
 python install.py
+pause
